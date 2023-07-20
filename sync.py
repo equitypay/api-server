44646cda660f461b10190ab6e666fe1008071497
volbil <volbilnexus@gmail.com>
2023-07-20 11:06:05 +0300

    Code cleanup

--- sync.py
+++ sync.py
@@ -7,5 +7,5 @@ from server.sync import sync_peers
 background = BlockingScheduler()
 # background.add_job(sync_mempool, "interval", seconds=30)
 background.add_job(sync_blocks, "interval", minutes=1)
-# background.add_job(sync_peers, "interval", minutes=1)
+background.add_job(sync_peers, "interval", minutes=1)
 background.start()
